690a3c040e6465ff3c68d740365752ead4f5bede
Igorik <egorsborsikov250575@gmail.com>
2025-03-17 16:41:39 +0700

    add description

--- backend/rest_controller/obj/Debug/net8.0/rest_controller.AssemblyInfo.cs
+++ backend/rest_controller/obj/Debug/net8.0/rest_controller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rest_controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa7cd643bf54f380393d7a06930a27560922d60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf73a6c9617a3567e2e95840fe6a1eb47d6d8b9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("rest_controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rest_controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
