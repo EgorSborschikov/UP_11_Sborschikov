abf59ccb01338eadc81bdc749b5329e20d5edd10
Igorik <egorsborsikov250575@gmail.com>
2025-03-25 17:24:33 +0700

    add lautout for read data in datagrid

--- frontend/VanessaApp/ViewModels/AdminWindowViewModel.cs
+++ frontend/VanessaApp/ViewModels/AdminWindowViewModel.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using ReactiveUI;
 using VanessaApp.Entities;
 
@@ -9,143 +11,57 @@ namespace VanessaApp.ViewModels
     {
         private readonly VanessaDbContext _context;
 
-        public ObservableCollection<employee> Employees { get; set; }
-        public ObservableCollection<position> Positions { get; set; }
-
+        // Поле для выбранного сотрудника
         private employee _selectedEmployee;
         public employee SelectedEmployee
         {
             get => _selectedEmployee;
-            set { this.RaiseAndSetIfChanged(ref _selectedEmployee, value); }
+            set => this.RaiseAndSetIfChanged(ref _selectedEmployee, value);
         }
 
-        private string _surname;
-        public string Surname
+        // Коллекция сотрудников
+        private ObservableCollection<employee> _employees;
+        public ObservableCollection<employee> Employees
         {
-            get => _surname;
-            set { this.RaiseAndSetIfChanged(ref _surname, value); }
+            get => _employees;
+            private set => this.RaiseAndSetIfChanged(ref _employees, value); // Сделаем сеттер приватным
         }
 
-        private string _name;
-        public string Name
-        {
-            get => _name;
-            set { this.RaiseAndSetIfChanged(ref _name, value); }
-        }
-
-        private string _lastName;
-        public string LastName
-        {
-            get => _lastName;
-            set { this.RaiseAndSetIfChanged(ref _lastName, value); }
-        }
-
-        private string _passportData;
-        public string PassportData
-        {
-            get => _passportData;
-            set { this.RaiseAndSetIfChanged(ref _passportData, value); }
-        }
-
-        private string _phoneNumber;
-        public string PhoneNumber
-        {
-            get => _phoneNumber;
-            set { this.RaiseAndSetIfChanged(ref _phoneNumber, value); }
-        }
-
-        private string _email;
-        public string Email
-        {
-            get => _email;
-            set { this.RaiseAndSetIfChanged(ref _email, value); }
-        }
-
-        private position _selectedPosition;
-        public position SelectedPosition
-        {
-            get => _selectedPosition;
-            set { this.RaiseAndSetIfChanged(ref _selectedPosition, value); }
-        }
-
-        private string _login;
-        public string Login
-        {
-            get => _login;
-            set { this.RaiseAndSetIfChanged(ref _login, value); }
-        }
-
-        private string _password;
-        public string Password
-        {
-            get => _password;
-            set { this.RaiseAndSetIfChanged(ref _password, value); }
-        }
-
-        private bool _isEmployeePanelVisible;
-        public bool IsEmployeePanelVisible
-        {
-            get => _isEmployeePanelVisible;
-            set { this.RaiseAndSetIfChanged(ref _isEmployeePanelVisible, value); }
-        }
-
+        // Конструктор
         public AdminWindowViewModel()
         {
             _context = new VanessaDbContext();
-            LoadData();
+            Employees = new ObservableCollection<employee>();
+            LoadEmployees();
         }
 
-        private void LoadData()
+        // Метод для получения сотрудников из базы данных
+        private IQueryable<employee> GetEmployees()
         {
-            Employees = new ObservableCollection<employee>(_context.employees.ToList());
-            Positions = new ObservableCollection<position>(_context.positions.ToList());
+            return _context.employees
+                .Include(e => e.IDBranchNavigation)
+                .Include(e => e.IDPositionNavigation)
+                .Include(e => e.IDAuthNavigation);
         }
 
-        public void SaveEmployee()
+        // Метод для загрузки сотрудников
+        private void LoadEmployees()
         {
-            if (SelectedEmployee == null)
+            try
             {
-                var newEmployee = new employee
+                // Получаем сотрудников и заполняем коллекцию
+                var employeesQuery = GetEmployees().ToList();
+                Employees.Clear(); // Очищаем коллекцию перед добавлением новых данных
+                foreach (var emp in employeesQuery)
                 {
-                    Surname = Surname,
-                    Name = Name,
-                    LastName = LastName,
-                    PassportData = PassportData,
-                    PhoneNumber = PhoneNumber,
-                    Email = Email,
-                    IDPosition = SelectedPosition?.IDPosition ?? 0,
-                    IDAuthNavigation = new auth
-                    {
-                        Login = Login,
-                        Password = Password
-                    }
-                };
-                _context.employees.Add(newEmployee);
+                    Employees.Add(emp);
+                }
+                Console.WriteLine($"Loaded {Employees.Count} employees.");
             }
-            else
+            catch (Exception ex)
             {
-                SelectedEmployee.Surname = Surname;
-                SelectedEmployee.Name = Name;
-                SelectedEmployee.LastName = LastName;
-                SelectedEmployee.PassportData = PassportData;
-                SelectedEmployee.PhoneNumber = PhoneNumber;
-                SelectedEmployee.Email = Email;
-                SelectedEmployee.IDPosition = SelectedPosition?.IDPosition ?? 0;
-                SelectedEmployee.IDAuthNavigation.Login = Login;
-                SelectedEmployee.IDAuthNavigation.Password = Password;
-
-                _context.employees.Update(SelectedEmployee);
+                Console.WriteLine($"Error loading employees: {ex.Message}");
             }
-            _context.SaveChanges();
-            LoadData();
-            IsEmployeePanelVisible = false; // Скрыть панель после сохранения
-        }
-
-        public void DeleteEmployee(employee employee)
-        {
-            _context.employees.Remove(employee);
-            _context.SaveChanges();
-            LoadData();
         }
     }
 }
